e089d5a7b5fb61ffc6a741cb1365eb8564f1665b
DrCausa <leo10said@gmail.com>
2025-09-17 10:51:47 -0500

    feat: implement genres fetching with loading and error handling in GenresPage

--- packages/app/src/pages/genres/index.tsx
+++ packages/app/src/pages/genres/index.tsx
@@ -1,5 +1,30 @@
+import { getGenres } from "@/services/api/genre";
+import type { Genre } from "@/services/api/types";
+import { useEffect, useState } from "react";
+
 const GenresPage = () => {
-  const genres = ["Realismo mágico", "Novela", "Ciencia ficción", "Fantasía"];
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchAuthors = async () => {
+      try {
+        setLoading(true);
+        const result = await getGenres();
+        setGenres(result);
+      } catch (err: any) {
+        setError(err.message || "Error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAuthors();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
     <div className="p-6">
@@ -7,10 +32,10 @@ const GenresPage = () => {
       <div className="flex flex-wrap gap-4">
         {genres.map((genre) => (
           <span
-            key={genre}
+            key={genre.id}
             className="bg-gradient-to-r from-purple-500 to-pink-500 text-white px-4 py-2 rounded-full shadow hover:scale-105 transition"
           >
-            {genre}
+            {genre.name}
           </span>
         ))}
       </div>
