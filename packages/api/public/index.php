22533f9b922a534e7ee6dd87f4ee81497a81baaf
DrCausa <leo10said@gmail.com>
2025-09-17 10:46:35 -0500

    feat: add CORS middleware for handling cross-origin requests

--- packages/api/public/index.php
+++ packages/api/public/index.php
@@ -18,6 +18,20 @@ AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
+$app->add(function ($request, $handler) {
+  $response = $handler->handle($request);
+
+  return $response
+    ->withHeader("Access-Control-Allow-Origin", "http://localhost:5173")
+    ->withHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+    ->withHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+    ->withHeader("Access-Control-Allow-Credentials", "true");
+});
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+  return $response;
+});
+
 $collector = $app->getRouteCollector();
 $collector->setDefaultInvocationStrategy(new RequestResponseArgs);
 
